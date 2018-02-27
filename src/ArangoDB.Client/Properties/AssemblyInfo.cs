095a3fcaebd4a02527060007fe6392d46579a139
Marko Zerdin <marko@zanyants.com>
2018-02-27 20:57:34 +0000

    - ArangoDB.Client now creates a package. - All three assemblies are now signed.

--- src/ArangoDB.Client/Properties/AssemblyInfo.cs
+++ src/ArangoDB.Client/Properties/AssemblyInfo.cs
@@ -7,7 +7,17 @@ using System.Runtime.InteropServices;
 [assembly: Guid("A03E3606-5617-481B-AE49-AFB52FA5C087")]
 #endif
 
-[assembly: InternalsVisibleTo("ArangoDB.Client.Test")]
-[assembly: InternalsVisibleTo("ArangoDB.Client.Try")]
+[assembly: InternalsVisibleTo( "ArangoDB.Client.Test, PublicKey=" +
+    "002400000480000094000000060200000024000052534131000400000100010081c30dcc79f4eb" +
+    "ebd2591e63f8669a21f5fd3fdfca06699883ffdbb22930ab5855ae87bc6e4e7ee6240a74b82acb" +
+    "e5331e8870f1af47ded94d96e99467756ad5abed514f04c5fac10acc82c66c8fd5f699c6b49816" +
+    "b0e69f6d947332efdfb1bf75ba66343c6be7fd510d4d3ea14d81f454c62b3f30321d124c7af380" +
+    "f15e9ddc" )]
+[assembly: InternalsVisibleTo( "ArangoDB.Client.Try, PublicKey=" +
+    "002400000480000094000000060200000024000052534131000400000100010081c30dcc79f4eb" +
+    "ebd2591e63f8669a21f5fd3fdfca06699883ffdbb22930ab5855ae87bc6e4e7ee6240a74b82acb" +
+    "e5331e8870f1af47ded94d96e99467756ad5abed514f04c5fac10acc82c66c8fd5f699c6b49816" +
+    "b0e69f6d947332efdfb1bf75ba66343c6be7fd510d4d3ea14d81f454c62b3f30321d124c7af380" +
+    "f15e9ddc" )]
 
 //[assembly: CLSCompliant(true)]
